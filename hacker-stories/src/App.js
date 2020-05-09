d27d9a6b5e8d6699025df15fa4bb50a03630625f
Lizzie <elizabethsiancrockett@gmail.com>
2020-05-09 21:40:08 +0100

    updates

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://reduxjs.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
-const List = () => {
-  return list.map((item) => {
+const List = (props) => {
+  return props.list.map((item) => {
     return (
       <div key={item.objectID}>
         <span><a href={item.url}>{item.title} </a></span>
@@ -33,37 +15,46 @@ const List = () => {
   })
 };
 
-const SearchButton = () => {
-  return (
-    <button id="searchButton">This is where you search</button>
-  )
-};
 
-const Search = () => {
+
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://reduxjs.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('[search term not entered]');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
   return (
-    <div id="searchBar">
+    <div>
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text"></input>
-      <SearchButton />
-    </div>
-  );
-}
-
-const Title = () => {
-  return (
-    <h1>My Hacker Stories</h1>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <Title />
-      <Search />
-
+      <input id="search" type="text" onChange={handleChange} />
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
       <hr />
 
-      <List />
+      <List list={stories} />
+
 
     </div>
   );
